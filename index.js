4577dc0546bf6b00744ab8ba179048f45be49197
colastar <lvjianwy@yeah.net>
2021-07-22 10:18:22 +0800

    push

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * @description:
+ * @author: manble@live.com
+ * @created: 2019-12-09
+ */
+ 'use strict'
+
+ module.exports = {
+   newsWap: 'http://3g.163.com',
+   live: 'http://data.live.126.net',
+   active: 'http://active.163.com',
+   cm: 'https://c.m.163.com',
+   starWap: 'https://star.3g.163.com',
+ 
+   // service
+   newsWapService: 'http://newswap.service.163.org',
+   // newsWapService: 'http://noah-wap3g-new-varnish-docker-cm-online:10000',
+   ncService: 'http://nc.service.163.org',
+   ncHzService: 'http://nc.hz.service.163.org',
+   newsWapNginxService: 'http://nginx.wap3g.service.163.org',
+   recommendHzService: 'http://relate-article.hz.service.163.org',
+ 
+   // test
+   newsWapTest: 'https://test.3g.163.com',
+ 
+   setRequestHost: [['http://noah-wap3g-new-varnish-docker-cm-online', '3g.163.com']],
+ }
+ 
